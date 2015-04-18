bd800f91760c0e3ee6c83189cf4a5fd9af9c57dd
Ebrahim Kobeissi <ebrahim.kobeissi@gmail.com>
2015-04-17 23:05:00 -0400

    be ruby

--- lib/josh.rb
+++ lib/josh.rb
@@ -3,65 +3,65 @@ require 'yaml'
 
 module Josh
 
-    Config = OpenStruct.new
-    Config.root = File.expand_path("#{File.dirname(__FILE__)}/..")
+  Config = OpenStruct.new
+  Config.root = File.expand_path("#{File.dirname(__FILE__)}/..")
 
-    class Bot
+  class Bot
 
-        attr_reader :config
+    attr_reader :config
 
-        def initialize()
-            load_pre_config
-            @config = Hash.new
-        end
-
-        def boot!
-            load_settings
-            load_plugins
-            start
-        end
-
-        def load_pre_config
-            require 'rubygems'
-            require 'bundler/setup'
-            Bundler.require(:default)
-        end
-
-        def load_settings
-            begin
-                data = YAML.load(File.open("#{Josh::Config.root}/config/settings.yaml"))
-                config.update(symbolize_keys_deep(data))
-            rescue SystemCallError
-                raise "Couldn't find settings.yaml"
-            end
-        end
-
-        def load_plugins
-            Dir.glob("#{Josh::Config.root}/plugins/*/*.rb").each { |lib_file| require lib_file }
-        end
-
-        def start
-            bot = Cinch::Bot.new
-            bot.configure do |c|
-                c.load config
-            end
-
-            bot.start
-        end
-
-        private
-
-        def symbolize_keys_deep(h)
-            hash = Hash.new
-            h.keys.each do |k|
-                ks = k.respond_to?(:to_sym) ? k.to_sym : k
-                if h[k].kind_of? Hash
-                    hash[ks] = symbolize_keys_deep h[k]
-                else
-                    hash[ks] = h[k]
-                end
-            end
-            hash
-        end
+    def initialize()
+      load_pre_config
+      @config = Hash.new
     end
+
+    def boot!
+      load_settings
+      load_plugins
+      start
+    end
+
+    def load_pre_config
+      require 'rubygems'
+      require 'bundler/setup'
+      Bundler.require(:default)
+    end
+
+    def load_settings
+      begin
+        data = YAML.load(File.open("#{Josh::Config.root}/config/settings.yaml"))
+        config.update(symbolize_keys_deep(data))
+      rescue SystemCallError
+        raise "Couldn't find settings.yaml"
+      end
+    end
+
+    def load_plugins
+      Dir.glob("#{Josh::Config.root}/plugins/*/*.rb").each { |lib_file| require lib_file }
+    end
+
+    def start
+      bot = Cinch::Bot.new
+      bot.configure do |c|
+        c.load config
+      end
+
+      bot.start
+    end
+
+    private
+
+    def symbolize_keys_deep(h)
+      hash = Hash.new
+      h.keys.each do |k|
+        ks = k.respond_to?(:to_sym) ? k.to_sym : k
+        if h[k].kind_of? Hash
+          hash[ks] = symbolize_keys_deep h[k]
+        else
+          hash[ks] = h[k]
+        end
+      end
+      hash
+    end
+  end
 end
--- plugins/huggle/huggle.rb
+++ plugins/huggle/huggle.rb
@@ -1,15 +1,15 @@
 class Huggle
-    include Cinch::Plugin
+  include Cinch::Plugin
 
-    match /huggle(?:\s*)(.*)/
-    def execute(m, target)
-        case target
-        when @bot.nick
-            m.action_reply "huggles himself"
-        when '', 'me'
-            m.action_reply "huggles #{m.user.nick}"
-        else
-            m.action_reply "huggles #{target}"
-        end
+  match /huggle(?:\s*)(.*)/
+  def execute(m, target)
+    case target
+    when @bot.nick
+     m.action_reply "huggles himself"
+    when '', 'me'
+      m.action_reply "huggles #{m.user.nick}"
+    else
+      m.action_reply "huggles #{target}"
     end
+  end
 end
