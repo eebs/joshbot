603e7922ea1eb7e26baca25ca6fc15f7f51af99b
unknown <JoeC@NPT-1153-WP.avi.com>
2016-02-29 15:19:37 -0500

    Accept CSV list of tickers in stock plugin

--- plugins/stocks/stocks.rb
+++ plugins/stocks/stocks.rb
@@ -7,20 +7,27 @@ class Stocks
   end
 
   match /stock\s*(.+)$/, method: :stock
-  def stock(m, symbol)
-    url = @symbols_url % symbol
-    result = HTTPClient.new.get(url)
-    if result.ok?
-      symbolDetails = JSON.parse(result.content)
-      if symbolDetails['list']['meta']['count'] == 1
-        fields = symbolDetails['list']['resources'].first['resource']['fields']
-        m.reply CGI.unescapeHTML(fields['issuer_name']) +
-          ": $%.2f" % fields['price'] +
-          " ↕%.2f" % fields['change'] +
-            "(%.1f%%)" % fields['chg_percent'] +
-          " http://finance.yahoo.com/q?s=#{symbol}"
+  def stock(m, symbols)
+    output = ''
+    symbols.split(",").each do |symbol|
+      symbol.strip!
+      url = @symbols_url % symbol
+      result = HTTPClient.new.get(url)
+      if result.ok?
+        symbolDetails = JSON.parse(result.content)
+        if symbolDetails['list']['meta']['count'] == 1
+          fields = symbolDetails['list']['resources'].first['resource']['fields']
+          output += CGI.unescapeHTML(fields['issuer_name']) +
+            ": $%.2f" % fields['price'] +
+            " ↕%.2f" % fields['change'] +
+              "(%.1f%%)" % fields['chg_percent'] +
+            " http://finance.yahoo.com/q?s=#{symbol}\n"
+        else
+          output += "No ticker found for symbol: #{symbol}\n"
+        end
       end
     end
+    m.reply output
   end
 
 end
